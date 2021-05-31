b9b97b1f661a4f7412162fbccf98086a7d88ea38
Tawfiek <tawfiek.108@gmail.com>
2021-05-31 13:08:30 +0200

    ui: add StartExam component ui

--- src/app/components/StartExam.tsx
+++ src/app/components/StartExam.tsx
@@ -1,9 +1,33 @@
 import { Component,  ReactNode } from "react";
+import { Col, Row, Spinner, Button, Form, Card } from "react-bootstrap";
 
 export class StartExam extends Component {
     render(): ReactNode {
         return (
-            <div>This is the first component to render</div>
+            <Card >
+                 <Card.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="example@example.com" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="userName">
+                            <Form.Label>Student Name</Form.Label>
+                            <Form.Control type="text" placeholder="Student Name" />
+                        </Form.Group>
+
+
+                        <Button variant="primary" type="submit">
+                        Submit
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+
         )
     }
 }
