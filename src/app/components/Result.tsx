af74db97172a6d23245b5979905d6bcaa09d7ef9
Tawfiek <tawfiek.108@gmail.com>
2021-05-31 16:19:24 +0200

    ui: add results component ui

--- src/app/components/Result.tsx
+++ src/app/components/Result.tsx
@@ -3,7 +3,10 @@ import { Component, ReactNode } from "react";
 export class Result extends Component {
     render(): ReactNode {
         return (
-            <div>Results Component !! </div>
+            <div className="result">
+                <h2>You've Done </h2>
+                <h3>Your Score 5 / 5</h3>
+            </div>
         );
     }
 }
